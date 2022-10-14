fd2ea9b05bafce046a81930ed27f9db30acbdda9
Pratik_Pandya <Pratik_Pandya@epam.com>
2022-10-14 17:02:57 +0530

    Commit for adding GetAPIData() overloaded versions in DataHelper Class

--- EPAM_TAO_CORE_API_TAF/GenericHelpers/DataHelper.cs
+++ EPAM_TAO_CORE_API_TAF/GenericHelpers/DataHelper.cs
@@ -4,14 +4,37 @@ using System.IO;
 using System.Reflection;
 using ExcelDataReader;
 using EPAM_TAO_CORE_UI_TAF.UI_Helpers;
+using System.Collections.Generic;
+using Newtonsoft.Json;
 
 namespace EPAM_TAO_CORE_API_TAF.APIHelpers
 {
     public class DataHelper
     {
         private static readonly object syncLock = new object();
+        private static DataHelper _dataHelper = null;
 
-        public static DataTable ExcelToDataTable(string strFileName, string strSheetName)
+        DataHelper()
+        {
+
+        }
+
+        public static DataHelper dataHelper
+        {
+            get
+            {
+                lock (syncLock)
+                {
+                    if (_dataHelper == null)
+                    {
+                        _dataHelper = new DataHelper();
+                    }
+                    return _dataHelper;
+                }
+            }
+        }
+
+        public DataTable GetAPIData(string strFileName, string strSheetName)
         {
             try
             {
@@ -43,6 +66,27 @@ namespace EPAM_TAO_CORE_API_TAF.APIHelpers
                 ErrorLogger.errorLogger.ErrorLog(MethodBase.GetCurrentMethod().Name, ex);
                 throw ex;
             }             
+        }
+
+        public string GetAPIData(object instance, Dictionary<string, dynamic> dictOfReqData)
+        {
+            try
+            {
+                Type type = instance.GetType();
+
+                foreach (var item in dictOfReqData)
+                {
+                    PropertyInfo propertyInfo = type.GetProperty(item.Key);
+                    propertyInfo.SetValue(instance, item.Value, null);
+                }
+
+                return JsonConvert.SerializeObject(instance);
+            }
+            catch (Exception ex)
+            {
+                ErrorLogger.errorLogger.ErrorLog(MethodBase.GetCurrentMethod().Name, ex);
+                throw ex;
+            }
         }
     }
 }
